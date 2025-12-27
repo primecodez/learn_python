ae2d008be7df6b359666b26974d1a7b76e39b6d6
primecodez <narendrakumarsingh2859@gmail.com>
2025-12-27 10:49:04 +0000

    Added few more details about list in python

--- 04_data_structures/01_lists.py
+++ 04_data_structures/01_lists.py
@@ -27,3 +27,29 @@ print(mixed[2])
   mixed.insert(2,10)   #insert at index
   print(mixed)
   
+#removing elements
+# 5️⃣ Removing elements
+numbers.remove(7)         # Remove by value
+last_item = numbers.pop()  # Remove last element
+print(numbers)
+print("Popped:", last_item)
+
+#find length of list using len() function 
+"""Here i an example of len() function"""
+num=[1,5,45,41]
+print("Length:",len(num))
+
+#How to do List slicing 
+"""Here is an example"""
+n=[1,5,4,5,78,15,16,98]
+print(n[1:4]) #it prints from index 1 to 3
+print(n[::3])#prints first 3 elements
+print(n[::-2])#prints every second element in reverse order starting from last element
+
+#Common list methods
+p=[1,5,66,45,78,41,15]
+p.sort()
+print("Sorted:",p)#sorts elements in ascending order
+
+p.reverse()
+print("Reversed:",p)#prints list in backward
