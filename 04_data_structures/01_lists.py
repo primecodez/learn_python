37847e4e17c2e7426ef53d941355c5a21d832f15
primecodez <narendrakumarsingh2859@gmail.com>
2025-12-27 06:33:07 +0000

    Added lists file

---
+++ 04_data_structures/01_lists.py
@@ -0,0 +1,29 @@
+""" Topic: Lists in python
+   - An ordered, changeable collection of items.
+   -Lists in Python are displayed using square brackets []
+   -They are mutable(can be changed)
+   -Are ordered 
+   -It allows duplicate elements
+
+"""
+#Creating lists
+numbers=[1,4,4,5,7]
+mixed=[1,3.14,False,"telephone"]   
+
+print(numbers)
+print(mixed)
+
+#accesing elements in a list
+print(nummber[0]])
+print(mixed[2])
+ 
+ #modifying elements in list
+ number[1]=6
+ print(number)
+
+ #adding elements
+ nummber.append(7)   #add at end
+ print(number)
+  mixed.insert(2,10)   #insert at index
+  print(mixed)
+  
